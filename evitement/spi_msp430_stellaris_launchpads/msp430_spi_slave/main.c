e82ab97567bac991ea0e390b87a72ab734f95d75
Erwan Martin <erwan@martin.tl>
2013-02-14 17:42:07 +0100

    Validation et commentaires test SPI launchpads

--- evitement/spi_msp430_stellaris_launchpads/msp430_spi_slave/main.c
+++ evitement/spi_msp430_stellaris_launchpads/msp430_spi_slave/main.c
@@ -1,27 +1,41 @@
 #include <msp430.h>
 
+/*
+ * Test de communication SPI entre un kit EK-LM4F120XL et un kit
+ * MSP-EXP430G2.
+ * 2013-02-14 Erwan Martin <erwan@martin.tl>
+ *
+ * Pineout:
+ * EK-LM4F120X  -> MSP-EXP430G2
+ * PD0 (SSICLK) -> P1.4 (UCA0CLK)
+ * PD2 (SSIRX)  -> P1.1 (UCA0SOMI)
+ * PD3 (SSITX)  -> P1.2 (UCA0SIMO)
+ */
+
 int main(void)
 {
-  WDTCTL = WDTPW + WDTHOLD;                 // Stop watchdog timer
-  while (!(P1IN & BIT4));
+  WDTCTL = WDTPW + WDTHOLD;  // Stop watchdog timer
+  while (!(P1IN & BIT4));    // Attente SPI
   
+  //Configuration sorties
   P1SEL = BIT1 + BIT2 + BIT4;
   P1SEL2 = BIT1 + BIT2 + BIT4;
-  UCA0CTL1 = UCSWRST;                       // **Put state machine in reset**
-  UCA0CTL0 = UCCKPH | UCCKPL | UCSYNC | UCMSB;      // 3-pin, 8-bit SPI slave
-  UCA0CTL1 &= ~UCSWRST;                     // **Initialize USCI state machine**
-  IE2 |= UCA0RXIE;                          // Enable USCI0 RX interrupt
+  
+  //Configuration UCA0
+  UCA0CTL1 = UCSWRST; // **Put state machine in reset**
+  UCA0CTL0 = UCCKPH | UCCKPL | UCSYNC | UCMSB; // 3-pin, 8-bit SPI slave, Phase and polarity
+  UCA0CTL1 &= ~UCSWRST; // **Initialize USCI state machine**
+  IE2 |= UCA0RXIE; // Enable USCI0 RX interrupt
 
-	while(1) {
-  		__bis_SR_register(LPM4_bits + GIE);       // Enter LPM4, enable interrupts
-	}
+  while(1) {
+      __bis_SR_register(LPM4_bits + GIE);       // Enter LPM4, enable interrupts
+  }
 }
 
-// Echo character
 #pragma vector=USCIAB0RX_VECTOR
 __interrupt void USCI0RX_ISR (void)
 {
-  while (!(IFG2 & UCA0TXIFG));              // USCI_A0 TX buffer ready?
-  unsigned int lol = UCA0RXBUF;
-  UCA0TXBUF = lol;
+  while (!(IFG2 & UCA0TXIFG)); // USCI_A0 TX buffer ready?
+  unsigned int lol = UCA0RXBUF; //Reception
+  UCA0TXBUF = lol; //Emission
 }
--- evitement/spi_msp430_stellaris_launchpads/stellaris_spi_master/main.c
+++ evitement/spi_msp430_stellaris_launchpads/stellaris_spi_master/main.c
@@ -12,11 +12,24 @@
 #include "inc/hw_gpio.h"
 #include "driverlib/gpio.h"
 
+/*
+ * Test de communication SPI entre un kit EK-LM4F120XL et un kit
+ * MSP-EXP430G2.
+ * 2013-02-14 Erwan Martin <erwan@martin.tl>
+ *
+ * Pineout:
+ * EK-LM4F120X  -> MSP-EXP430G2
+ * PD0 (SSICLK) -> P1.4 (UCA0CLK)
+ * PD2 (SSIRX)  -> P1.1 (UCA0SOMI)
+ * PD3 (SSITX)  -> P1.2 (UCA0SIMO)
+ */
+
 void main(void) {
 
     SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
                        SYSCTL_XTAL_16MHZ);
 
+    //Configuration SPI
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI1);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
     GPIOPinConfigure(GPIO_PD0_SSI1CLK);
@@ -24,17 +37,23 @@ void main(void) {
     GPIOPinConfigure(GPIO_PD2_SSI1RX);
     GPIOPinConfigure(GPIO_PD3_SSI1TX);
     GPIOPinTypeSSI(GPIO_PORTD_BASE,
-    		GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
+            GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
     SSIConfigSetExpClk(SSI1_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_3,
                        SSI_MODE_MASTER, 1000, 8);
     SSIEnable(SSI1_BASE);
 
-    unsigned char lol = 0xCD;
+    unsigned char data = 0xCD;
     while(1) {
-    	//lol = lol == 0xCD ? 0xCE : 0xCD;
-    	SSIDataPut(SSI1_BASE, lol);
-    	unsigned long pulData;
-    	SSIDataGet(SSI1_BASE, &pulData);
-    	SysCtlDelay(1000000);
+        data++;
+
+        //Envoie
+        SSIDataPut(SSI1_BASE, data);
+        unsigned long pulData;
+
+        //Reception
+        SSIDataGet(SSI1_BASE, &pulData);
+
+        //Delai
+        SysCtlDelay(1000000);
     }
 }
