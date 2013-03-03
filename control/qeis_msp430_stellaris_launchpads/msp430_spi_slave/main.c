cb4fab7e682fc52eb3bafca75e6b11612b22ff83
Erwan Martin <erwan@martin.tl>
2013-03-03 19:40:34 +0100

    Transfert des quadratures et des erreurs entre les deux launchpads, mais qui marche vraiment

--- control/qeis_msp430_stellaris_launchpads/msp430_spi_slave/main.c
+++ control/qeis_msp430_stellaris_launchpads/msp430_spi_slave/main.c
@@ -7,33 +7,154 @@
  *
  * Deux commandes distinctes permettent de récupérer soit
  * 12 octets (quadratures) soit 12 octets (erreurs).
+ * 
+ * Le stellaris peut simuler une entrée quadrature sur
+ * P2.0 et P2.1 
  *
  * Pineout:
  * EK-LM4F120X  -> MSP-EXP430G2
  * PD0 (SSICLK) -> P1.4 (UCA0CLK)
  * PD2 (SSIRX)  -> P1.1 (UCA0SOMI)
  * PD3 (SSITX)  -> P1.2 (UCA0SIMO)
+ * PE1 (PE1)    -> P1.5 (P1.5)
+ * PE2 (PE2)    -> P2.0 (P2.0)
+ * PE3 (PE3)    -> P2.1 (P2.1)
  */
 
-unsigned char spi_buffer[13];
+#define ETAT_1 1
+#define ETAT_2 2
+#define ETAT_3 3
+#define ETAT_4 4
+
+unsigned char spi_buffer[12];
 unsigned char *spi_p = spi_buffer;
 unsigned char spi_state = 0;
 
-int main(void) {
-  WDTCTL = WDTPW + WDTHOLD; //Stop watchdog timer
-  while (!(P1IN & BIT4));
-  
-  //Pineout
-  P1SEL = BIT1 + BIT2 + BIT4;
-  P1SEL2 = BIT1 + BIT2 + BIT4;
+unsigned int qs[6];
+unsigned int etats[6];
+unsigned int errs[6];
 
+void handle_input(unsigned char index, unsigned char inputs) {
+	if ( qs[index] == 65535 ){ 
+		qs[index] = 0;
+	}
+
+    switch (etats[index]){
+    case ETAT_1:
+    	if (inputs == 0x02){
+    		qs[index]++;
+    		etats[index] = 2;
+    		break;
+    	}
+		if (inputs == 0x01){
+			qs[index]--;
+			etats[index] = 4;
+			break;
+		}
+		if (inputs & 0x03){
+			errs[index]++;
+			etats[index] = 3;
+			break;
+		}
+		break;
+    case ETAT_2:
+    	if (inputs == 0x03){
+    		qs[index]++;
+    		etats[index] = 3;
+    		break;
+    	}
+		if (inputs == 0x00){
+			qs[index]--;
+			etats[index] = 1;
+			break;
+		}
+		if (inputs == 0x01){
+			errs[index]++;
+			etats[index] = 4;
+			break;
+		}
+		break; 
+    case ETAT_3:
+    	if (inputs == 0x01){
+    		qs[index]++;
+    		etats[index] = 4;
+    		break;
+    	}
+		if (inputs == 0x02){
+			qs[index]--;
+			etats[index] = 2;
+			break;
+		}
+		if (inputs == 0x00){
+			errs[index]++;
+			etats[index] = 1;
+			break;
+		}
+		break;    	
+    case ETAT_4:
+    	if (inputs == 0x00){
+    		qs[index]++;
+    		etats[index] = 1;
+    		break;
+    	}
+		if (inputs == 0x03){
+			qs[index]--;
+			etats[index] = 3;
+			break;
+		}
+		if (inputs == 0x02){
+			errs[index]++;
+			etats[index] = 2;
+			break;
+		}
+		break;
+    }
+}
+
+#pragma vector = PORT1_VECTOR
+__interrupt void P1_ISR(void) {
   //Config SPI
-  UCA0CTL1 = UCSWRST;                    //Reste state machine
+  UCA0CTL1 = UCSWRST;                    //Reset state machine
   UCA0CTL0 |= UCCKPL | UCMSB + UCSYNC;   //Config SPI
   UCA0CTL1 &= ~UCSWRST;                  //Start state machine
   IE2 |= UCA0RXIE | UCA0TXIE;            //Enable RX + TX interrupts
+  
+  spi_p = spi_buffer;
 
-  while(1) __bis_SR_register(LPM0_bits + GIE);
+  P1IFG &= ~BIT5;                        //Clear interrupt
+}
+
+int main(void) {
+  WDTCTL = WDTPW + WDTHOLD; //Stop watchdog timer
+  
+  DCOCTL = CALDCO_16MHZ;       // DCO frequency set to 8 MHz
+  BCSCTL1 = CALBC1_16MHZ;      // DCO range set to 8 MHz
+  
+  //Pineout SPI
+  P1SEL = BIT1 + BIT2 + BIT4;
+  P1SEL2 = BIT1 + BIT2 + BIT4;
+  //Config CS SPI
+  P1IES |= BIT5;   // high -> low is selected with IES.x = 1.
+  P1IFG &= ~BIT5;  // To prevent an immediate interrupt, clear the flag for
+                   // P1.5 before enabling the interrupt.
+  P1IE |= BIT5;    // Enable interrupts for P1.5
+
+
+  //Init values
+  unsigned char i;
+  for(i=6; i--; ) {
+  	qs[i] = 0;
+  	errs[i] = 0;
+  	etats[i] = 1;
+  }
+
+  __enable_interrupt();
+  
+  while(1) { 
+  	handle_input(0, P2IN & 0x03); //P2.0 + P2.1
+  	handle_input(1, (P2IN >> 2) & 0x03); //P2.2 + P2.3
+  	handle_input(2, (P2IN >> 4) & 0x03); //P2.4 + P2.5
+  }
 }
 
 #pragma vector=USCIAB0RX_VECTOR
@@ -44,18 +165,18 @@ __interrupt void USCI0RX_ISR (void)
 
   if (data == 0x55) {
   	//On demande les quadratures
-  	spi_buffer[0]  = 0x11;
-  	spi_buffer[1]  = 0x22;
-  	spi_buffer[2]  = 0x33;
-  	spi_buffer[3]  = 0x44;
-  	spi_buffer[4]  = 0x55;
-  	spi_buffer[5]  = 0x66;
-  	spi_buffer[6]  = 0x77;
-  	spi_buffer[7]  = 0x88;
-  	spi_buffer[8]  = 0x99;
-  	spi_buffer[9]  = 0xAA;
-  	spi_buffer[10] = 0xBB;
-  	spi_buffer[11] = 0xCC;
+  	spi_buffer[0]  = qs[0] & 0xFF;
+  	spi_buffer[1]  = (qs[0] >> 8) & 0xFF;
+  	spi_buffer[2]  = qs[1] & 0xFF;
+  	spi_buffer[3]  = (qs[1] >> 8) & 0xFF;
+  	spi_buffer[4]  = qs[2] & 0xFF;
+  	spi_buffer[5]  = (qs[2] >> 8) & 0xFF;
+  	spi_buffer[6]  = qs[3] & 0xFF;
+  	spi_buffer[7]  = (qs[3] >> 8) & 0xFF;
+  	spi_buffer[8]  = qs[4] & 0xFF;
+  	spi_buffer[9]  = (qs[4] >> 8) & 0xFF;
+  	spi_buffer[10] = qs[5] & 0xFF;
+  	spi_buffer[11] = (qs[5] >> 8) & 0xFF;
   	spi_p = spi_buffer;
   	spi_state = 1;
   	UCA0TXBUF = *spi_p;
@@ -63,18 +184,18 @@ __interrupt void USCI0RX_ISR (void)
   }
   if (data == 0xAA) {
   	//On demande les erreurs
-  	spi_buffer[0]  = 0x01;
-  	spi_buffer[1]  = 0x02;
-  	spi_buffer[2]  = 0x04;
-  	spi_buffer[3]  = 0x04;
-  	spi_buffer[4]  = 0x05;
-  	spi_buffer[5]  = 0x06;
-  	spi_buffer[6]  = 0x07;
-  	spi_buffer[7]  = 0x08;
-  	spi_buffer[8]  = 0x09;
-  	spi_buffer[9]  = 0x0A;
-  	spi_buffer[10] = 0x0B;
-  	spi_buffer[11] = 0x0C;
+  	spi_buffer[0]  = errs[0] & 0xFF;
+  	spi_buffer[1]  = (errs[0] >> 8) & 0xFF;
+  	spi_buffer[2]  = errs[1] & 0xFF;
+  	spi_buffer[3]  = (errs[1] >> 8) & 0xFF;
+  	spi_buffer[4]  = errs[2] & 0xFF;
+  	spi_buffer[5]  = (errs[2] >> 8) & 0xFF;
+  	spi_buffer[6]  = errs[3] & 0xFF;
+  	spi_buffer[7]  = (errs[3] >> 8) & 0xFF;
+  	spi_buffer[8]  = errs[4] & 0xFF;
+  	spi_buffer[9]  = (errs[4] >> 8) & 0xFF;
+  	spi_buffer[10] = errs[5] & 0xFF;
+  	spi_buffer[11] = (errs[5] >> 8) & 0xFF;
   	spi_p = spi_buffer;
   	spi_state = 1;
   	UCA0TXBUF = *spi_p;
--- control/qeis_msp430_stellaris_launchpads/stellaris_spi_master/main.c
+++ control/qeis_msp430_stellaris_launchpads/stellaris_spi_master/main.c
@@ -22,22 +22,29 @@
  * Deux commandes distinctes permettent de récupérer soit
  * 12 octets (quadratures) soit 12 octets (erreurs).
  *
+ * PE2 et PE3 simulent une sortie quadrature, pour tester
+ * le bon fonctionnement du MSP
+ *
+ * Le fait de resetter les SPI des deux cartes est important,
+ * car sinon il risque d'y avoir des décalages dans les
+ * registres, et ainsi avoir des bugs aléatoires liés par
+ * exemple à l'ordre dans lequel on a allumé les cartes.
+ *
  * Pineout:
  * EK-LM4F120X  -> MSP-EXP430G2
  * PD0 (SSICLK) -> P1.4 (UCA0CLK)
  * PD2 (SSIRX)  -> P1.1 (UCA0SOMI)
  * PD3 (SSITX)  -> P1.2 (UCA0SIMO)
+ * PE1 (PE1)    -> P1.5 (P1.5)
+ * PE2 (PE2)    -> P2.0 (P2.0)
+ * PE3 (PE3)    -> P2.1 (P2.1)
  */
 
 unsigned short qeis[6];
 unsigned short errs[6];
 
-void main(void) {
-	//Config sys
-    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
-                       SYSCTL_XTAL_16MHZ);
-
-    //Configuration SPI
+void spi_reset_init(void) {
+	SysCtlPeripheralReset(SYSCTL_PERIPH_SSI1);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_SSI1);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD);
     GPIOPinConfigure(GPIO_PD0_SSI1CLK);
@@ -49,10 +56,49 @@ void main(void) {
     SSIConfigSetExpClk(SSI1_BASE, SysCtlClockGet(), SSI_FRF_MOTO_MODE_3,
                        SSI_MODE_MASTER, 1000, 8);
     SSIEnable(SSI1_BASE);
+}
+
+void main(void) {
+	//Config sys
+    SysCtlClockSet(SYSCTL_SYSDIV_4 | SYSCTL_USE_PLL | SYSCTL_OSC_MAIN |
+                       SYSCTL_XTAL_16MHZ);
+    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOE);
+
+    //Simulation quadrature
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_3);
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_2);
+
+    //SPI CS
+    GPIOPinTypeGPIOOutput(GPIO_PORTE_BASE, GPIO_PIN_1);
+    GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_PIN_1);
+
+    unsigned int dt = 100000;
 
 	while(1) {
+		unsigned int ii;
+		for (ii = 0; ii<10; ii++) {
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_PIN_3);
+		    SysCtlDelay(dt);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, GPIO_PIN_2);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, GPIO_PIN_3);
+		    SysCtlDelay(dt);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, GPIO_PIN_2);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
+		    SysCtlDelay(dt);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_2, 0);
+			GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_3, 0);
+		    SysCtlDelay(dt);
+		}
+
 		unsigned char *p;
 		unsigned long in;
+
+		//SPI configure
+		spi_reset_init();
+		//SPI CS, High -> Low
+		GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, 0);
+		SysCtlDelay(1000);
 
 		//récupération des quadratures
 		p = (unsigned char *)qeis;
@@ -78,6 +124,9 @@ void main(void) {
 			*(p++) = in & 0xFF;
 			SysCtlDelay(1000);
 		}
+
+		//SPI CS, Low => High
+		GPIOPinWrite(GPIO_PORTE_BASE, GPIO_PIN_1, GPIO_PIN_1);
 
 		//la patience est une vertue qui s'aquiert avec de la patience
 		SysCtlDelay(1000000);
